d8c2c36068f87567bdb90683c9b01c477924514e
Benjamin Nisler <bnisler@gmail.com>
2019-10-18 18:45:30 -0500

    first commit

---
+++ src/build/precache-manifest.5a4110c4078b5e9d7f8ffe99dca7496b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f643fa907f1241911797dd5ee9d536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d29ea6ecaa4e8124c493",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "b9ae8b32df4e127645ba",
+    "url": "/static/js/2.66a0e807.chunk.js"
+  },
+  {
+    "revision": "d29ea6ecaa4e8124c493",
+    "url": "/static/js/main.ff2e14a0.chunk.js"
+  },
+  {
+    "revision": "16f57bc6dde3f2278832",
+    "url": "/static/js/runtime~main.82f07dd6.js"
+  }
+]);
